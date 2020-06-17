9fb88235a719af4b58e53321ec20175d93117e0a
wayter95 <wayter.paulo.95@gmail.com>
2020-06-17 11:08:15 -0300

    Ajuste register page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,27 +1,39 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import './style.css';
-import {FiMail} from 'react-icons/fi';
+import { FiMail, FiLock,FiFacebook} from 'react-icons/fi';
 import logo from '../../assets/logo-login.svg';
 
 const Login = () => {
     return (
-       <div id="page-login">
-           <div className="container">
-           <img src={logo} alt="Autenticação Animada"/>
-           <div className="content">
-               <form>
-                   <input type="email" placeholder="E-mail"/>
-                   <FiMail size="20"/>
-                   <input type="password" placeholder="Senha"/>
-                   <a href="">Esqueci minha senha</a>
-                   <button className="btn">ENTRAR</button>
-                   <label >_________________________ ou _________________________</label>
-                   <button className="btn">FACEBOOK</button>
-                   <a href="">Não tem uma conta? <strong>Registre-se</strong></a>
-               </form>
-           </div>
-           </div>
-       </div>
+        <div id="page-login">
+            <div className="container">
+                <img src={logo} alt="Autenticação Animada" />
+                <div className="content">
+                    <form>
+                        <div className="InputWithIcons">
+                            <label className="icon">
+                                <FiMail size="30"/>
+                            </label>
+                            <input type="email" placeholder="E-mail" />
+                        </div>
+                        <div className="InputWithIcons">
+                            <label className="icon">
+                                <FiLock size="30"/>
+                            </label>
+                            <input type="password" placeholder="Senha" />
+                        </div>
+                        <Link to="/">Esqueci minha senha</Link>
+                        <button className="btn" id="entrar" type="submit">ENTRAR</button>
+                        <label >_________________________ ou _________________________</label>
+                        <button className="btn" type="button" disabled>
+                            <FiFacebook />
+                            FACEBOOK</button>
+                        <Link to="/register">Não tem uma conta? <strong>Registre-se</strong></Link>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import { FiMail, FiLock,FiUser,FiCalendar} from 'react-icons/fi';
+import logo from '../../assets/logo-login.svg';
+
 import './style.css';
 
 const Register = () => {
     return (
-        <h1>Hellow Word</h1>
+        <div id="page-register">
+        <div className="container">
+            <img src={logo} alt="Autenticação Animada" />
+            <div className="content">
+                <form>
+                <div className="InputWithIcons">
+                        <label className="icon">
+                            <FiUser size="30"/>
+                        </label>
+                        <input type="text" placeholder="Nome" />
+                    </div>
+                    <div className="InputWithIcons">
+                        <label className="icon">
+                            <FiMail size="30"/>
+                        </label>
+                        <input type="email" placeholder="E-mail" />
+                    </div>
+                    <div className="InputWithIcons">
+                        <label className="icon">
+                            <FiLock size="30"/>
+                        </label>
+                        <input type="password" placeholder="Senha" />
+                    </div>
+                    <div className="InputWithIcons">
+                        <label className="icon">
+                            <FiCalendar size="30"/>
+                        </label>
+                        <input type="date" placeholder="Data de Nascimento" />
+                    </div>
+                    <button className="btn" id="entrar" type="submit">CONTINUAR</button>
+                   
+                    <Link to="/">Voltar</Link>
+                </form>
+            </div>
+        </div>
+    </div>
     )
 }
 
