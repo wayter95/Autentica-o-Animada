eb384ca0376ab0b2963737c5f345201fc3670adb
wayter95 <wayter.paulo.95@gmail.com>
2020-06-18 11:10:50 -0300

    ajuster css login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
-import { FiMail, FiLock,FiFacebook} from 'react-icons/fi';
+import { Link } from 'react-router-dom'
+import { FiMail, FiLock, FiFacebook } from 'react-icons/fi';
 import logo from '../../assets/logo-login.svg';
 
 import './style.css';
@@ -8,31 +8,29 @@ import './style.css';
 const Login = () => {
     return (
         <div id="page-login">
-            <div className="container">
-                <img src={logo} alt="Autenticação Animada" />
-                <div className="content">
-                    <form>
-                        <div className="InputWithIcons">
-                            <label className="icon">
-                                <FiMail size="30"/>
-                            </label>
-                            <input type="email" placeholder="E-mail" />
-                        </div>
-                        <div className="InputWithIcons">
-                            <label className="icon">
-                                <FiLock size="30"/>
-                            </label>
-                            <input type="password" placeholder="Senha" />
-                        </div>
-                        <Link to="/">Esqueci minha senha</Link>
-                        <button className="btn" id="entrar" type="submit">ENTRAR</button>
-                        <label >_________________________ ou _________________________</label>
-                        <button className="btn" type="button" disabled>
-                            <FiFacebook />
+            <img src={logo} alt="Autenticação Animada" />
+            <div className="content">
+                <form>
+                    <div className="InputWithIcons">
+                        <label className="icon">
+                            <FiMail size="30" />
+                        </label>
+                        <input type="email" placeholder="E-mail" />
+                    </div>
+                    <div className="InputWithIcons">
+                        <label className="icon">
+                            <FiLock size="30" />
+                        </label>
+                        <input type="password" placeholder="Senha" />
+                    </div>
+                    <Link to="/">Esqueci minha senha</Link>
+                    <button className="btn" id="entrar" type="submit">ENTRAR</button>
+                    <label>_________________________ ou _________________________</label>
+                    <button className="btn" type="button" disabled>
+                        <FiFacebook />
                             FACEBOOK</button>
-                        <Link to="/register">Não tem uma conta? <strong>Registre-se</strong></Link>
-                    </form>
-                </div>
+                    <Link to="/register">Não tem uma conta? <strong>Registre-se</strong></Link>
+                </form>
             </div>
         </div>
     )
