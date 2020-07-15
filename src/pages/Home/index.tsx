4fae75ca076ab9e8c244a9457b0c520909e9b553
wayter95 <wayter.paulo.95@gmail.com>
2020-07-15 15:55:04 -0300

    using skeleton in page home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Skeleton from "react-loading-skeleton";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
 
@@ -9,11 +10,21 @@ const Home = () => {
       <Sidebar />
       <Header title="Dashboard" />
       <div className="content">
-        <div className="widgets"></div>
-        <div className="widgets"></div>
-        <div className="widgets"></div>
-        <div className="graph"></div>
-        <div className="notifications"></div>
+        <div className="widgets">
+          <Skeleton count={1} height={200} />
+        </div>
+        <div className="widgets">
+          <Skeleton count={1} height={200} />
+        </div>
+        <div className="widgets">
+          <Skeleton count={1} height={200} />
+        </div>
+        <div className="graph">
+          <Skeleton count={1} height={700} />
+        </div>
+        <div className="notifications">
+          <Skeleton count={1} height={700} />
+        </div>
       </div>
     </div>
   );
